2cc4d42332f251913b0d7e8d23c22ffdfde3f41f
Eby <ebinshaji22@gmail.com>
2026-08-23 15:06:27 +0530

    Initial project upload

--- src/app/components/SplashScreen.tsx
+++ src/app/components/SplashScreen.tsx
@@ -89,7 +89,7 @@ export function SplashScreen() {
                 style={{ fontFamily: "'Cormorant Garamond', serif" }}
                 className="text-white text-[80px] md:text-[100px] leading-none tracking-tight"
               >
-                G
+                V
               </motion.span>
 
               <motion.span
@@ -106,7 +106,7 @@ export function SplashScreen() {
                 style={{ fontFamily: "'Cormorant Garamond', serif" }}
                 className="text-white text-[80px] md:text-[100px] leading-none tracking-tight"
               >
-                V
+                G
               </motion.span>
             </motion.div>
 
